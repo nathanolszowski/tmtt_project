9b4996290b8687332eec167b753baf6a5e1c7fdb
nathan <olszowski.nathan@outlook.fr>
2019-11-19 22:36:58 +0100

    travail sur fichiers config + core + logs

--- ttmt_pkg/config.py
+++ ttmt_pkg/config.py
@@ -2,32 +2,68 @@
 # -- FICHIER DE CONFIGURATIONS --
 # ----------------------------------------
 
-# Ce fichier configure les repertoires utilisés dans le programme
+# Ce fichier configure le projet TMTT.
+
 
 import os
+# import time
 
+
+"""
+Fonctions gestion des chemins
+"""
 
 chemins = dict(
-    cheminProjet='/home/nathan/Bureau/TTMT_projectttmt_project',
+    cheminProjet='/home/nathan/Bureau/TTMT_project/ttmt_project',
     # cheminJava='',
-    cheminDicts='',
-    cheminCorpus='',
-    cheminFiches=''
+    cheminDicts='/home/nathan/Bureau/TTMT_project/dictionnaire',
+    cheminCorpus='/home/nathan/Bureau/TTMT_project/corpus',
+    cheminFiches='/home/nathan/Bureau/TTMT_project/fiches',
+    cheminLogs='/home/nathan/Bureau/TTMT_project/logs'
 )
 
 
-def TestChemin(chemin):
-        # valider ou non accès aux fichiers
+def test_chemin(cheminRepo):
+    """
+    Permet de valider ou non accès à un dossier/fichier
+    :param cheminRepo : Chemin vers un dossier/fichier
+    :return : <str> Validation ou Non
+    """
     try:
-        os.path.exists(chemin)
-        print('Le chemin vers le répertoire' + chemin + 'a été trouvé')
+        os.path.exists(cheminRepo)
+        print('Le chemin vers le répertoire ' + cheminRepo + ' a été trouvé')
     except NameError:
-        print('!!Le chemin vers le répertoire' + chemin +
+        print('!!Le chemin vers le répertoire' + cheminRepo +
               'est introuvable. Vérifiez vos chemins dans votre fichier de configuration!!')
 
 
-def TestAcces(cheminsRepo):
-        # valider accès à tous les dossiers
-    for item in cheminsRepo:
-        TestChemin(item)
+def test_tous_chemins(cheminsRepo):
+    """
+    Permet de valider accès à tous les dossiers/fichiers
+    :param : <dict> Dictionnaire des chemins
+    :return : <str> Validation ou non test_chemin()
+    """
+    for item in cheminsRepo.items():
+        test_chemin(str(item))
         print('\n')
+
+
+"""
+Fonctions gestion des temps de traitements
+"""
+
+"""
+def chrono(ChronoS):
+    q, s = divmod(ChronoS, 60)
+    h, m = divmod(q, 60)
+    j, r = divmod(h, 24)
+    if j > 0:
+        TempsRestantJHMS = "%d_j %d_h %d_min %d_s" % (j, h, m, s)
+    elif h > 0:
+        TempsRestantJHMS = "%d_h %d_min %d_s" % (h, m, s)
+    elif m > 0:
+        TempsRestantJHMS = "%d_min %d_s" % (m, s)
+    else:
+        TempsRestantJHMS = "%d_s" % (s)
+    return TempsRestantJHMS
+"""
--- ttmt_pkg/logs_fdl.py
+++ ttmt_pkg/logs_fdl.py
@@ -3,3 +3,32 @@
 Ecriture des logs et fiche de lecture
 
 """
+
+import config as cfg
+import csv
+
+
+def lecture_CSV(nom, chemin=str(cfg.chemins['cheminLogs'])):
+    """
+    Permet la lecture d'un fichier CSV
+    """
+    fichierCSV = '/' + chemin + nom
+    cfg.test_chemin(fichierCSV)
+    print('Ouverture du fichier ' + nom)
+
+    file = open(nom, 'r')
+    reader = csv.reader(file, delimiter=';')
+    for row in reader:
+        listeMots = set(row)
+    file.close()
+    return listeMots
+
+
+def nouveau_csv(nom, chemin=str(cfg.chemins['cheminLogs'])):
+    """
+    Permet la création d'un fichier CSV
+    """
+    nom = chemin + '/' + nom + '.csv'
+    file = open(nom, "wb")
+    file.close()
+    return 'Le fichier ' + nom + ' a été créé'
